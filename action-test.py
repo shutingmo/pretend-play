b6ed8f3456619ecef243cb5f29cc9c38d60b9b66
Shuting Mo <42949696+shutingmo@users.noreply.github.com>
2019-02-17 15:48:10 -0500

    Update action-test.py

--- action-test.py
+++ action-test.py
@@ -1,95 +1,26 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+# encoding: utf-8
+import paho.mqtt.client as mqtt 
+import json
 
-import configparser
-from hermes_python.hermes import Hermes
-from hermes_python.ffi.utils import MqttOptions
-from hermes_python.ontology import *
-import io
-import os
+def on_connect(client, userdata, flags, rc): 
+    print('Connected') 
+    mqtt.subscribe('hermes/intent/#')
 
-CONFIGURATION_ENCODING_FORMAT = "utf-8"
-CONFIG_INI = "config.ini"
-
-class SnipsConfigParser(configparser.SafeConfigParser):
-    def to_dict(self):
-        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
-
-
-def read_configuration_file(configuration_file):
-    try:
-        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
-            conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
-            return conf_parser.to_dict()
-    except (IOError, configparser.Error) as e:
-        return dict()
-
-def subscribe_intent_callback(hermes, intentMessage):
-    conf = read_configuration_file(CONFIG_INI)
-    action_wrapper(hermes, intentMessage, conf)
-
-
-def action_wrapper(hermes, intentMessage, conf):
-    voiceText = intentMessage.slots.wordsSlot.first().value # We extract the value from the slot "house_room"
-
-    result_sentence = ""
-
-    result_sentence = "You said a  : {}".format(str(voiceText))  # The response that will be said out loud by the TTS engine.
-
-    os.system("mpg321 welcome.mp3")
-    
-    current_session_id = intentMessage.session_id
-    hermes.publish_end_session(current_session_id, result_sentence)
-
-
-if __name__ == "__main__":
-    mqtt_opts = MqttOptions()
-    with Hermes(mqtt_options=mqtt_opts) as h:
-        h.subscribe_intent("{{intent_id}}", subscribe_intent_callback) \
-         .start()
-
-
-'''select = input("Enter letter selection: ")                  #2
-if select != "a" or select != "b" or select != "c":
-    select = incorrectInp(select)
-currDir = readData(currDir, select)
-playsound(select + '.wav')                                  # in a.wav, "This is A"
-finalSelect = finalSelect+select
-
-select = input("Enter word selection: ")                  #3
-currDir = readDataFinal(currDir, select)                     # in a.wav, "This is A", "This is B"
-finalSelect = finalSelect + select
-playsound(finalSelect + '.wav')'''
-
-#should loop to the original after reading final one
-#loop initial part
-
-
-#You CHOOSE cave and that'll give you the complete, concatenated string.
-#then go up a little in the directory, go into the final folder and play the wav there
-
-
-
-'''
-""" Write the body of the function that will be executed once the intent is recognized. 
-In your scope, you have the following objects : 
-- intentMessage : an object that represents the recognized intent
-- hermes : an object with methods to communicate with the MQTT bus following the hermes protocol. 
-- conf : a dictionary that holds the skills parameters you defined. 
-  To access global parameters use conf['global']['parameterName']. For end-user parameters use conf['secret']['parameterName'] 
- 
-Refer to the documentation for further details. 
-""" 
-
-
-house_room = intentMessage.slots.wordsSlot.first().value # We extract the value from the slot "house_room"
-
-result_sentence = ""
-
-result_sentence = "Awesome! You want to be a  : {}".format(str(house_room))  # The response that will be said out loud by the TTS engine.
-
-
-current_session_id = intentMessage.session_id
-hermes.publish_end_session(current_session_id, result_sentence)
-'''
+def on_message(client, userdata, msg):
+    # Parse the json response
+    intent_json = json.loads(msg.payload)
+    intentName = intent_json['intent']['intentName']
+    slots = intent_json['slots']
+    print('Intent {}'.format(intentName))
+    for slot in slots:
+        slot_name = slot['slotName']
+        raw_value = slot['rawValue']
+        value = slot['value']['value']
+        print('Slot {} -> \n\tRaw: {} \tValue: {}'.format(slot_name, raw_value, value))
+        
+mqtt = mqtt.Client() 
+mqtt.on_connect = on_connect
+mqtt.on_message = on_message
+mqtt.connect('raspberrypi.local', 1883) 
+mqtt.loop_forever()
